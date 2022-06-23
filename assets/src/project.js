e1024d6f3a5cdff17add28469467396a7c83a9f3
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-24 00:24:14 +0300

    Adding a fetch request in javascript

--- assets/src/project.js
+++ assets/src/project.js
@@ -4,7 +4,33 @@ function renderOnePoem(poem){
 let mainDiv = document.createElement('div')
 mainDiv.className = 'grid-container'
 mainDiv.innerHTML = `
+<div id="bigger-container">
+<div id="poet-key">
+  <img
+    src="https://pbs.twimg.com/profile_images/1518574167444144130/gbxEiBIy_400x400.jpg"
+    alt="poet image"
+    id="poet-image"
+  />
+  <div id="poets-details">
+    <h2 id="poet-name">Author's Name: Martin Kamau</h2>
+    <br />
+    <a href="#" target="blank" id="poet-url">Link Up With Author</a>
+  </div>
+</div>
 
+<div id="poem-container">
+  <h2 id="poetry-title">Hello</h2>
+  <p id="poetry-content">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+    alias quam
+  </p>
+  <a href="" id="poetry-url">Read more on the poem</a>
+</div>
+</div>
 
 `
+
+//Add poem to DOM
+
+document.querySelector('#grid-container').appendChild(mainDiv)
 }
